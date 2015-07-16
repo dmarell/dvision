eec67bfa7c94be807e1aa1f9bfc7214f1741d495
Daniel Marell <daniel.marell@cag.se>
2015-07-16 14:14:33 +0200

    Volume for storing grabbed images.

--- dvision-server/src/main/deploy/docker-run.sh
+++ dvision-server/src/main/deploy/docker-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ex
-docker stop dvision-server || true
-docker rm -f dvision-server || true
 docker build -t dvision-server .
+docker stop dvision-server 2> /dev/null || true
+docker rm -f dvision-server || true
 docker run -d --name dvision-server \
     -p 14562:8080 \
     -v $(mkdir -p logs;cd logs; pwd):/logs \
-    -v /mnt/raid/public/cams/cam-motion-grabber:/cam-motion-grabber
+    -v /mnt/raid/public/cams/cam-motion-grabber:/cam-motion-grabber \
     dvision-server
